322b689f96edd49bfdeadfd81605823c5b029148
pravarntle <pravarntle@gmail.com>
2023-07-27 22:46:06 +0700

    Initial commit

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,66 @@
+<template>
+  <CSidebar
+    position="fixed"
+    :unfoldable="sidebarUnfoldable"
+    :visible="sidebarVisible"
+    @visible-change="
+      (event) =>
+        $store.commit({
+          type: 'updateSidebarVisible',
+          value: event,
+        })
+    "
+  >
+    <CSidebarBrand>
+      <CImage
+        :src="logo"
+        fluid
+        block
+      />
+      <!-- <CIcon
+        custom-class-name="sidebar-brand-full"
+        :icon="logoNegative"
+        :height="35"
+      />
+      <CIcon
+        custom-class-name="sidebar-brand-narrow"
+        :icon="sygnet"
+        :height="35"
+      /> -->
+    </CSidebarBrand>
+    <AppSidebarNav />
+    <CSidebarToggler
+      class="d-none d-lg-flex"
+      @click="$store.commit('toggleUnfoldable')"
+    />
+  </CSidebar>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { AppSidebarNav } from './AppSidebarNav'
+import logo  from '@/assets/images/logo.jpg'
+import { logoNegative } from '@/assets/brand/logo-negative'
+import { sygnet } from '@/assets/brand/sygnet'
+export default {
+  name: 'AppSidebar',
+  components: {
+    AppSidebarNav,
+  },
+  data() {
+    return {
+      logo: logo,
+    }
+  },
+  setup() {
+    const store = useStore()
+    return {
+      logoNegative,
+      sygnet,
+      sidebarUnfoldable: computed(() => store.state.sidebarUnfoldable),
+      sidebarVisible: computed(() => store.state.sidebarVisible),
+    }
+  },
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  // use this option for production linking
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue/demo/4.0/' : '/',
+  configureWebpack: {
+    //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
+    resolve: {
+      symlinks: false,
+    },
+  },
+})
