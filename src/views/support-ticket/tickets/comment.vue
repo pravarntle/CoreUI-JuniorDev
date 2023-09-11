055c7d2337e3ce79e4c0bcd9fb134007f428d712
64160062Settawut <142302637+Settawut0@users.noreply.github.com>
2023-09-11 23:15:34 +0700

    go
    g

--- src/views/support-ticket/tickets/comment.vue
+++ src/views/support-ticket/tickets/comment.vue
@@ -1,7 +1,24 @@
 <template>
-    
+    <mark>Tong ไม่ทำงาน </mark>
+    <br>
+     <button type="submit" value="ยืนยัน" onclick="myFunction()"> กด Submit เพื่อยืนยันคำตอบ </button>
+
 </template>
 <script>
+ export default {
+
+    methods: {
+     myFunction() {
+      alert("ตองขี้เกียจ");
+    }
+    },
+    components: {
+      CFormInput,
+      CIcon
+    }
+}
+
+
 </script>
 <style>
 </style>
