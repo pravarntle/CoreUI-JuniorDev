96034a5fe32b9416b603accc1ca18a50a1075e4f
pravarntle <pravarntle@gmail.com>
2023-09-14 11:08:43 +0700

    update mongoose for initial backend project

--- backend/routes/mongoose.js
+++ backend/routes/mongoose.js
@@ -3,31 +3,31 @@ var router = express.Router();
 const mongooseController = require('../controllers/mongooseController')
 
 /* GET users listing. */
-router.get('/accounts', mongooseController.find)
+// router.get('/accounts', mongooseController.find)
 
 // ROUTE EXAMPLE //
 
-// // GET //
-// router.get('/get/:model', mongooseController.find)
+// GET //
+router.get('/get/:model', mongooseController.find)
 
-// router.get('/getOne/:model/:id', mongooseController.findOne)
+router.get('/getOne/:model/:id', mongooseController.findOne)
 
-// // POST //
-// router.post('/insert/:model', mongooseController.save)
+// POST //
+router.post('/insert/:model', mongooseController.save)
 
-// router.post('/get/:model', mongooseController.find)
+router.post('/get/:model', mongooseController.find)
 
-// router.post('/getOne/:model/:id', mongooseController.findOne)
-// router.post('/getOne/:model', mongooseController.findOne)
+router.post('/getOne/:model/:id', mongooseController.findOne)
+router.post('/getOne/:model', mongooseController.findOne)
 
-// router.post('/aggregate/:model', mongooseController.aggregate)
+router.post('/aggregate/:model', mongooseController.aggregate)
 
-// // PUT //
-// router.put('/update/:model/:id', mongooseController.update)
-// router.put('/update/:model', mongooseController.update)
+// PUT //
+router.put('/update/:model/:id', mongooseController.update)
+router.put('/update/:model', mongooseController.update)
 
-// // DELETE //
-// router.delete('/delete/:model/:id', mongooseController.remove)
-// router.delete('/delete/:model', mongooseController.remove)
+// DELETE //
+router.delete('/delete/:model/:id', mongooseController.remove)
+router.delete('/delete/:model', mongooseController.remove)
 
 module.exports = router;
