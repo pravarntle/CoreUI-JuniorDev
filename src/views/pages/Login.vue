dbfaef848b7d86d37a8fc70715b707b3059c0daa
Sutthapat <sutthapat.b@gmail.com>
2023-12-13 20:00:09 +0700

    login v2.0.0 (ยังไม่เสร็จ)

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -1,69 +1,42 @@
 <template>
-    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CCardGroup>
-          <CCard>
-          <CCardBody>
-          <CRow>
-              <CCol>
-                <!-- <img :src="dataImageURL" width="50" height="50"/> -->
-                <CImage
-                  :src="login"
-                  fluid
-                  block
-                  class="c-image"
-                />
-              </CCol>
-              <CCol>
-                  <CForm>
-                    <h1 class="mt-5">LOGIN</h1>
-                    <CFormLabel>Username</CFormLabel>
-                    <CInputGroup class="mb-3">
-                      <CFormInput
-                        v-model="form.username"
-                        feedbackInvalid="ห้ามเว้นว่าง"
-                        :invalid="validate.username"
-                        size="lg"
-                        id="username"
-                      />
-                    </CInputGroup>
-                    <CFormLabel>Password</CFormLabel>
-                    <CInputGroup class="mb-4">
-                      <CFormInput
-                        id="password"
-                        :type="showPassword?'text':'password'"
-                        v-model="form.password"
-                        feedbackInvalid="ห้ามเว้นว่าง"
-                        :invalid="validate.password"
-                        autocomplete="current-password"
-                        size="lg"
-                      />
-                      <CInputGroupText>
-                        <CFormCheck
-                        type="radio"
-                        autocomplete="off"
-                        @click="showPassword=!showPassword"
-                        >
-                        <template #label><CIcon :icon="cilToggleOff" /></template>
-                        </CFormCheck>
-                      </CInputGroupText>
-                    </CInputGroup>
-                    <CFormCheck class="remember" id="rememberMe" label="Remember me"/>
-                    <CRow>
-                      <CCol :xs="6">
-                        <CButton color="dark" class="px-4" @click="onLoginClick"> Login </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-              </CCol>
-          </CRow>
-        </CCardBody>
-        </CCard>
-        </CCardGroup>
-      </CContainer>
-    </div>
+  <div>
+    <CCard class="Card">
+      <CCardBody>
+        <CRow>
+          <CCol>
+            <CForm>
+              <h1 class="mt-5">LOGIN</h1>
+              <CFormLabel>Username</CFormLabel>
+              <CInputGroup class="mb-3">
+                <CFormInput v-model="form.username" feedbackInvalid="ห้ามเว้นว่าง" :invalid="validate.username" size="lg"
+                  id="username" />
+              </CInputGroup>
+              <CFormLabel>Password</CFormLabel>
+              <CInputGroup class="mb-4">
+                <CFormInput id="password" :type="showPassword ? 'text' : 'password'" v-model="form.password"
+                  feedbackInvalid="ห้ามเว้นว่าง" :invalid="validate.password" autocomplete="current-password" size="lg" />
+                <CInputGroupText>
+                  <CFormCheck type="radio" autocomplete="off" @click="showPassword = !showPassword">
+                    <template #label>
+                      <CIcon :icon="cilToggleOff" />
+                    </template>
+                  </CFormCheck>
+                </CInputGroupText>
+              </CInputGroup>
+              <CFormCheck class="remember" id="rememberMe" label="Remember me" />
+              <CRow>
+                <CCol :xs="6">
+                  <CButton color="dark" class="px-4" @click="onLoginClick"> Login </CButton>
+                </CCol>
+              </CRow>
+            </CForm>
+          </CCol>
+        </CRow>
+      </CCardBody>
+    </CCard>
+  </div>
 
-    <!-- <CToaster placement="top-end">
+  <!-- <CToaster placement="top-end">
         <CToast visible color="primary" v-for="(toast) in toastProp">
             <CToastHeader closeButton v-if="toast.title">
                 <span class="me-auto fw-bold">{{ toast.title }}</span>
@@ -74,130 +47,149 @@
         </CToast>
     </CToaster> -->
 </template>
-<style>
+<style >
 .c-image {
-    border-radius: 15px 15px 15px 15px;
-  }
+  border-radius: 15px 15px 15px 15px;
+}
+
+body {
+  background-image: url('~@/assets/images/Background_Blue.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+  background-color: #F1F7F9;
+}
+
+.Card {
+  background-color: rgba(255, 255, 255, 0.5);
+  /* สีขาวโปร่งใส 50% */
+  width: 680px;
+  height: 650px;
+  position: absolute;
+  right: 50px;
+  top: 100px;
+}
 </style>
 <script>
 import axios from 'axios'
 import { CIcon } from '@coreui/icons-vue';
-import { cilToggleOff,cilToggleOn } from '@coreui/icons';
-import login  from '@/assets/images/login.jpg'
+import { cilToggleOff, cilToggleOn } from '@coreui/icons';
+import login from '@/assets/images/login.jpg'
 import { CFormInput } from '@coreui/vue-pro';
+import Background from '@/assets/images/Background_Blue.png'
 export default {
-    name: 'Login1',
-    data() {
-      return {
-        dataImageURL: '',
-        cilToggleOff,
-        cilToggleOn,
-        login: login,
-        validatedCustom01: null,
-        form: {
-          username: '',
-          password: '',
-        },
-        validate: {
-          username: null,
-          password: null,
-        },
-        showPassword:false,
+  name: 'Login1',
+  data() {
+    return {
+      dataImageURL: '',
+      cilToggleOff,
+      cilToggleOn,
+      login: login,
+      validatedCustom01: null,
+      form: {
+        username: '',
+        password: '',
+      },
+      validate: {
+        username: null,
+        password: null,
+      },
+      showPassword: false,
+      Background,
+      // toastProp: [],
+    };
+  },
+  created() {
+    this.getImage()
+    // this.toastProp.push({
+    //   content: 'OK'
+    // })
+  },
+  methods: {
+    async getImage() {
+      try {
+        const dataResponse = await axios.post(`${process.env.VUE_APP_URL}/mongoose/getOne/stts_files/652c0247d44e6b62f7b1f65f`)
+        this.dataImageURL = `data:${dataResponse.data.filetype};base64,${dataResponse.data.image}`
+      } catch (error) {
 
-        // toastProp: [],
-      };
+      }
     },
-    created() {
-      this.getImage()
-      // this.toastProp.push({
-      //   content: 'OK'
-      // })
+    vaildateBeforeSave() {
+      let error = false
+      if (this.form.username === '') {
+        error = true
+        this.validate.username = true
+      }
+      if (this.form.password === '') {
+        error = true
+        this.validate.password = true
+      }
+      return error
+      // if (error) {
+      // } else if(this.form.username === 'supakit' && this.form.password === '00000000') {
+      //   this.onLoginClick()
+      // }else{
+      // alert("เข้าสู่ระบบไม่สำเร็จ")
+      // }
+
     },
-    methods: {
-      async getImage() {
+    async onLoginClick() {
+      if (this.vaildateBeforeSave()) {
+
+
+      } else {
         try {
-          const dataResponse = await axios.post(`${process.env.VUE_APP_URL}/mongoose/getOne/stts_files/652c0247d44e6b62f7b1f65f`)
-          this.dataImageURL = `data:${dataResponse.data.filetype};base64,${dataResponse.data.image}`
+          const response = await axios.post(`${process.env.VUE_APP_URL}/auth/login`, { username: this.form.username, password: this.form.password })
+          console.log(response);
+          const user = {
+            id: response.data.user.id,
+            USERNAME: response.data.user.USERNAME,
+            role: response.data.user.role,
+            // role: response.data.data.role,  
+            token: response.data.user.token
+
+          }
+
+          localStorage.setItem('USER_DATA', JSON.stringify(user))
+          console.log(user.role)
+          setTimeout(function () {
+            if (user.role === 'Employee') {
+              this.$router.push('/support-ticket/user/dashboard');
+            } else if (user.role === 'Admin') {
+              this.$router.push('/support-ticket/admin/admin_dashboard');
+            } else if (user.role === 'ItSupport') {
+              this.$router.push('/support-ticket/it/it_dashboard');
+            } else if (user.role === 'Manager') {
+              this.$router.push('/support-ticket/manager/manager_dashboard');
+            }
+
+          }.bind(this), 1500)
+
         } catch (error) {
-          
-        }
-      },
-      vaildateBeforeSave() {
-        let error = false
-        if (this.form.username === '') {
-          error = true
-          this.validate.username = true
-        }
-        if (this.form.password === '') {
-          error = true
-          this.validate.password = true
-        }
-        return error
-        // if (error) {
-          // } else if(this.form.username === 'supakit' && this.form.password === '00000000') {
-          //   this.onLoginClick()
-        // }else{
-          // alert("เข้าสู่ระบบไม่สำเร็จ")
-        // }
-
-      },
-      async onLoginClick() {
-        if (this.vaildateBeforeSave()) {
-          
-
-        } else {
-          try {
-            const response = await axios.post(`${process.env.VUE_APP_URL}/auth/login`, { username: this.form.username, password: this.form.password })
-            console.log(response);
-            const user = {
-              id: response.data.user.id, 
-              USERNAME: response.data.user.USERNAME,
-              role:response.data.user.role,
-              // role: response.data.data.role,  
-              token: response.data.user.token
-              
-            }
-
-            localStorage.setItem('USER_DATA', JSON.stringify(user))
-            console.log(user.role)
-            setTimeout(function() {
-              if (user.role === 'Employee') {
-                this.$router.push('/support-ticket/user/dashboard');
-              } else if (user.role === 'Admin') {
-                this.$router.push('/support-ticket/admin/admin_dashboard');
-              } else if (user.role === 'ItSupport') {
-                this.$router.push('/support-ticket/it/it_dashboard');
-              } else if (user.role === 'Manager') {
-                this.$router.push('/support-ticket/manager/manager_dashboard');
-              }
-
-            }.bind(this), 1500)
-            
-          } catch (error) {
-            console.log(error)
-            if (error.response.status === 401) {
-              alert('รหัสผ่านไม่ถูกต้อง หรือ ไม่พบผู้ใช้'); // แสดง Alert ถ้ามีค่าสถานะ 401
-            }
-            if (error.response.status === 500) {
-              alert('เซิฟเวอร์มีปัญหา'); // แสดง Alert ถ้ามีค่าสถานะ 401
-            }
+          console.log(error)
+          if (error.response.status === 401) {
+            alert('รหัสผ่านไม่ถูกต้อง หรือ ไม่พบผู้ใช้'); // แสดง Alert ถ้ามีค่าสถานะ 401
+          }
+          if (error.response.status === 500) {
+            alert('เซิฟเวอร์มีปัญหา'); // แสดง Alert ถ้ามีค่าสถานะ 401
           }
         }
-        
-        // this.$router.push('/support-ticket/user/dashboard');
-      },
-      // showPassword(){
-      //   var x = document.getElementById("password");
-      //   if (x.type === "password") {
-      //     x.type = "text";
-      //   } else {
-      //     x.type = "password";
-      //   }
-      // },
+      }
+
+      // this.$router.push('/support-ticket/user/dashboard');
     },
-    components: {
-      CFormInput,
-      CIcon
-    }
+    // showPassword(){
+    //   var x = document.getElementById("password");
+    //   if (x.type === "password") {
+    //     x.type = "text";
+    //   } else {
+    //     x.type = "password";
+    //   }
+    // },
+  },
+  components: {
+    CFormInput,
+    CIcon
+  }
 }
 </script>
