8f112f3729a197ecac40669808367f9aae475c74
64160298 <yukonthorn02@gmail.com>
2023-12-22 00:08:18 +0700

    Admin dashboard finished

--- src/views/support-ticket/admin/admin_dashboard.vue
+++ src/views/support-ticket/admin/admin_dashboard.vue
@@ -1,21 +1,148 @@
   <template>
-    <div class="box">
+    <div class="box" >
       <CRow>
         <count_ticket/>
       </CRow>
+      <CRow >
+        <CRow>
+          <CCard style="width: 100%; height: auto; background: white; border-radius: 18px">
+            <CCardBody>
+              <CRow>
+                <CCol style="height: auto; width: 30px;">
+                  <h5>User Roles</h5>
+                  <div>
+                    <CIcon :icon="icon.cilCalendar" size="xxl"/>
+                    <label>&nbsp; | 26 Jul 2021 </label>
+                  </div>
+                  <CChart
+                    type="pie"
+                    class="d-flex"
+                    :data="{
+                      labels: [
+                      'Employee',
+                      'IT Support',
+                      'Admin',
+                      'Manager',
+                    ],
+                    datasets: [
+                      {
+                        backgroundColor: [
+                          '#0071FF',
+                          '#EE5731',
+                          '#F860E9',
+                          '#62BB1E',
+                        ],
+                        data: [40, 20, 10, 5],
+                      },
+                    ],
+                  }"
+                  :options="{
+                    plugins: {
+                      legend: {
+                        display: true,
+                        position: 'left',
+                        labels: {
+                          usePointStyle: true,
+                          fontSize: 12,
+                          radius: 0,
+                        },
+                      },
+                    },
+                  }"
+                />
+                </CCol>
+                <CCol class="vl"></CCol>
+                <CCol style="height: auto;">
+                  <h5>User Status</h5>
+                    <label>| now update</label>
+                  <CChart
+                        type="pie"
+                        class="d-flex"
+                        :data="{
+                          labels: [
+                            'Active',
+                            'Inactive'
+                          ],
+                          datasets: [
+                            {
+                              backgroundColor: [
+                                '#0071FF',
+                                '#EE5731'
+                              ],
+                              data: [60, 40],
+                            },
+                          ],
+                        }"
+                      :options="{
+                        plugins: {
+                          legend: {
+                            display: true,
+                            position: 'left',
+                            labels: {
+                              usePointStyle: true,
+                              fontSize: 12,
+                              radius: 0,
+                            },
+                          },
+                        },
+                      }"
+                    />
+                </CCol>
+              </CRow>
+            </CCardBody>
+          </CCard>
+        </CRow>
+        <CRow><br></CRow>
+        <CRow>
+          <CCard style="width: 100%; height: auto; background: white; border-radius: 18px">
+            <CCardBody>
+              <h1>Priority Category</h1>
+              <div>
+                <CIcon :icon="icon.cilCalendar" size="xxl"/>
+                <label>&nbsp; | 26 Jul 2021 </label>
+              </div>
+              <hr>
+              <PriorityChart />
+            </CCardBody>
+          </CCard>
+        </CRow>
+      </CRow>
+      <UserList />
     </div>
   </template>
-  
-  
-  <script>
-   import { CCol, CRow } from '@coreui/vue-pro'
-   import count_ticket from '@/components/CountTicket.vue'
+<style>
+.vl {
+  margin-left: 20%;
+  border-left: 3px solid rgba(194, 197, 204, .5);
+  height: auto;
+}
+</style>
+
+<script>
+  import { CIcon } from '@coreui/icons-vue';
+  import * as icon from '@coreui/icons';
+  import { CCard, CCardBody, CCol, CRow } from '@coreui/vue-pro'
+  import count_ticket from '@/components/CountTicket.vue'
+  import PriorityChart from '../../../components/PriorityBar.vue'
+  import UserList from '@/components/UserList.vue'
+  import { CChart } from '@coreui/vue-chartjs';
   export default {
-    name: 'amin_dashboard',
-  
-  
-      components: { CRow, CCol , count_ticket},
-      
+    name: 'admin_dashboard',
+    components: {
+      CRow,
+      CCol,
+      count_ticket,
+      PriorityChart,
+      CCard,
+      CCardBody,
+      UserList,
+      CIcon,
+      CChart
+    },
+    setup() {
+    return {
+      icon,
     }
-  </script>
-  
+  }
+}
+</script>
--- src/views/support-ticket/manager/manager_dashboard.vue
+++ src/views/support-ticket/manager/manager_dashboard.vue
@@ -1,7 +1,78 @@
 <template>
-    
+  <div class="box" style="width: 100%">
+    <CRow>
+      <count_ticket/>
+    </CRow>
+    <CRow style="height: auto;">
+      <CCol style="width: 30%;">
+        <CCard>
+          <CCardBody>
+            <h4>Support Tracker</h4>
+            <p>Last 7 Days</p>
+            <br>
+            <h1>164</h1>
+            <h4>Total Tickets</h4>
+            <br><br>
+          </CCardBody>
+        </CCard>
+
+      </CCol>
+      <CCol style="width: 30%;">
+        <CRow>
+          <CCard>
+            <CCardBody>
+              <CRow>
+                <CCol>
+                  <h1 stype="Bold">Type Chart</h1>
+                </CCol>
+                <CCol>
+                  <CChartDoughnut :data="defaultData"
+                  style="height: 300px; max-height: 300px; margin-top: 40px"/>
+                </CCol>
+              </CRow>
+            </CCardBody>
+        </CCard>
+        </CRow>
+        <CRow>
+          <h2>2nd line</h2>
+        </CRow>
+      </CCol>
+    </CRow>
+
+  </div>
+
 </template>
+
+
 <script>
+import { CCol, CRow} from '@coreui/vue-pro'
+import { CChartDoughnut } from '@coreui/vue-chartjs'
+import count_ticket from '@/components/CountTicket.vue'
+
+const doughnutLabel = {
+  id: 'doughnutLabel'
+}
+
+export default {
+  name: 'CChartDoughnut',
+  components: {
+    CRow,
+    CCol,
+    CChartDoughnut,
+    count_ticket },
+  computed: {
+    defaultData() {
+      return {
+        labels: ['VueJs', 'EmberJs', 'VueJs', 'AngularJs'],
+        datasets: [
+          {
+            backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+            data: [40, 20, 80, 10],
+          },
+        ],
+        plugins: [doughnutLabel]
+      }
+    },
+  },
+}
 </script>
-<style>
-</style>
