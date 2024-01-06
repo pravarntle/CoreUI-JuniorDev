835395d3c88d048a17daf31624b7a0dcafe03bcd
phutthipong <phutthipongza2545@gmail.com>
2024-01-06 19:45:21 +0700

    Update admin_dashboard.vue

--- src/views/support-ticket/admin/admin_dashboard.vue
+++ src/views/support-ticket/admin/admin_dashboard.vue
@@ -1,21 +1,48 @@
   <template>
-    <div class="box">
-      <CRow>
-        <count_ticket/>
-      </CRow>
-    </div>
-  </template>
+  <div class="box">
+    <CRow>
+      <count_ticket />
+    </CRow>
+
+    <CRow>
+      <CCol xs="12">
+        <CCard>
+          <CCard xs="6">
+            <CCardTitle>
+              <CButton
+                variant="ghost"
+                class="border border-0 bg-body ms-auto d-flex"
+                ><img :src="Close_fullscreen"
+              /></CButton>
+              
+              <CCard>
+                
+              </CCard>
+            </CCardTitle>
+          </CCard>
+        </CCard>
+      </CCol>
+    </CRow>
+  </div>
+</template>
   
   
   <script>
-   import { CCol, CRow } from '@coreui/vue-pro'
-   import count_ticket from '@/components/CountTicket.vue'
-  export default {
-    name: 'amin_dashboard',
-  
-  
-      components: { CRow, CCol , count_ticket},
-      
+import { CCol, CRow } from '@coreui/vue-pro'
+import count_ticket from '@/components/CountTicket.vue'
+import Open_in_full from '@/assets/images/open_in_full.png'
+import Close_fullscreen from '@/assets/images/close_fullscreen.png'
+import { CChart } from '@coreui/vue-chartjs'
+import { CIcon } from '@coreui/icons-vue'
+export default {
+  name: 'amin_dashboard',
+  components: { CRow, CCol, count_ticket, CIcon },
+  setup() {
+    return {
+      Open_in_full,
+      Close_fullscreen,
     }
-  </script>
+  },
+}
+</script>
   
