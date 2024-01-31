abd1a95825e37dcc47cbe1de6db03044a6eaa860
bankkamo926 <bankkamo926@gmail.com>
2024-01-31 22:42:37 +0700

    change

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   // use this option for production linking
-  publicPath: process.env.NODE_ENV === '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue/demo/4.0/' : '/',
   configureWebpack: {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
